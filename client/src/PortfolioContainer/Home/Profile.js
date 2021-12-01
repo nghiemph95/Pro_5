1c5344deaab292042335281bcf937dc133d5f1b2
nghiemph95 <nghiemph95@gmail.com>
2021-12-01 23:31:00 +0700

    update2

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Typical from "react-typical";
 import Button from "react-bootstrap/Button";
+import "./Profile.css";
 
 export default function Profile() {
   return (
@@ -8,21 +9,23 @@ export default function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.facebook.com/nghiempham47/">
-              <i className="fa fa-facebook-square"></i>
-            </a>
-            <a href="#">
-              <i className="fa fa-google-plus-square"></i>
-            </a>
-            <a href="https://www.instagram.com/nghiemilo/?hl=en">
-              <i className="fa fa-instagram"></i>
-            </a>
-            <a href="#">
-              <i className="fa fa-youtube-square"></i>
-            </a>
-            <a href="#">
-              <i className="fa fa-twitter"></i>
-            </a>
+            <div className="colz-icon">
+              <a href="https://www.facebook.com/nghiempham47/">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="https://myaccount.google.com/profile">
+                <i className="fa fa-google-plus-square"></i>
+              </a>
+              <a href="https://www.instagram.com/nghiemilo/?hl=en">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://www.youtube.com/channel/UC5ZlCHQqHL2qEclS6bfe_ag">
+                <i className="fa fa-youtube-square"></i>
+              </a>
+              <a href="https://twitter.com/NghiemPh95">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
           </div>
 
           <div className="profile-details-name">
@@ -59,12 +62,15 @@ export default function Profile() {
             </span>
           </div>
 
-          <div className="profile-option">
-            <button className="btn primary-bth"> Hire Me </button>
+          <div className="profile-options">
+            <button className="btn primary-btn"> Hire Me </button>
             <a href="NG95.pdf" download="Nghiem_Pham NG95.pdf">
               <Button className="btn highlighted-btn">Get Resume</Button>
             </a>
           </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
