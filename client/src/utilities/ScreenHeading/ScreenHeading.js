d5cf921d8e2a66192c672acd095d6a9749d05cf8
nghiemph95 <nghiemph95@gmail.com>
2021-12-03 12:15:47 +0700

    commit

---
+++ client/src/utilities/ScreenHeading/ScreenHeading.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ScreenHeading.css";
+
+export default function ScreenHeading(props) {
+  return (
+    <div className="heading-container">
+      <div className="screen-heading">
+        <span>{props.title}</span> {/** Receiving value */}
+      </div>
+
+      {props.subHeading ? (
+        <div className="screen-sub-heading"> {/** Why choose me */}
+          <span>{props.subHeading}</span>
+        </div>
+      ) : (
+        <div></div>
+      )}
+
+      <div className="heading-seperator">
+        <div className="seperator-line">
+          <div className="seperator-blod">
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
